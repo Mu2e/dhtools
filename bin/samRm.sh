bf196e8deb3f901d2b390fd1cdafd209302a4c32
Ray Culbertson <rlc@fnal.gov>
2022-06-22 17:46:58 -0500

    add retire-only option

--- bin/samRm.sh
+++ bin/samRm.sh
@@ -11,8 +11,9 @@ echo "
    samRm [OPTIONS] [-f FILE]  [-s FILEOFNAMES] [-d DATASET]
       -n interpret file lists, but don't actually do the delete
       -h print help
-  
-     Permanently remove files from SAM database and 
+      -r only retire file records in SAM, do not delete in dCache
+
+     Permanently remove files from SAM database and
      retire them in enstore (the tape system).
      You can re-upload a new file of the same name.
      The space on tape may eventually be overwritten.
@@ -32,7 +33,7 @@ rmFile() {
 
   if ! LOCS=`samweb locate-file $FN` ; then
      echo "ERROR: could not run locate on $FN"
-    exit 1  
+    exit 1
   fi
 
 
@@ -43,7 +44,7 @@ $LOCS"
     exit 1
   fi
 
-  if [ $LOCS ]; then
+  if [[ -n "$LOCS"  && "$RMLOC" == "yes" ]]; then
     MEDIA=`echo $LOCS | awk -F: '{print $1}'`
     PNFS=` echo $LOCS | awk -F: '{print $2}' | sed -e 's/([^()]*)//g' `
     PNFS=$PNFS/$FN
@@ -60,7 +61,7 @@ echo $LOCS"
      echo "ERROR: error during rm $PNFS"
      exit 1
     fi
-    
+
   fi # end has a location
 
   echo "retiring $FN"
@@ -82,8 +83,9 @@ export FILEOPT=""
 export DSOPT=""
 export FOFOPT=""
 export DOIT="yes"
+export RMLOC="yes"
 
-while getopts d:f:s:hn OPT; do
+while getopts d:f:s:hnr OPT; do
     case $OPT in
         f)
             export FILEOPT=$OPTARG
@@ -100,6 +102,9 @@ while getopts d:f:s:hn OPT; do
             ;;
         n)
             export DOIT="no"
+            ;;
+        r)
+            export RMLOC="no"
             ;;
         *)
             echo unknown option, exiting
@@ -120,7 +125,7 @@ fi
 export SAM_EXPERIMENT=mu2e
 
 # will need sam_web_client, setup if not already there
-[ -z "$SETUP_SAM_WEB_CLIENT" ] && setup sam_web_client 
+[ -z "$SETUP_SAM_WEB_CLIENT" ] && setup sam_web_client
 
 TMP=/tmp/${USER}_samrm_$$
 touch $TMP
@@ -132,7 +137,7 @@ touch $TMP
 [ "$FOFOPT" != "" ] && cat $FOFOPT >> $TMP
 
 # for file names provided with -d
-if [ "$DSOPT" != ""  ]; 
+if [ "$DSOPT" != ""  ];
 then
   samweb list-files "dh.dataset=$DSOPT" >> $TMP
 fi
@@ -140,16 +145,21 @@ fi
 N=`cat $TMP | wc -l`
 
 if [ $N -le 0 ]; then
-  echo Exiting - No files found 
+  echo Exiting - No files found
   usage
   rm -f $TMP
   exit 1
 fi
 
-echo "Will delete $N files in sam and enstore, examples:"
+if [ "$RMLOC" == "yes" ]; then
+    echo "Will delete $N files in SAM and enstore, examples:"
+else
+    echo "Will retire $N files in SAM, examples:"
+fi
+
 cat $TMP | head -5
 
-if [ "$DOIT" != "yes" ]; 
+if [ "$DOIT" != "yes" ];
 then
   exit 0
 fi
